a8ba6c7cbb2a54b7bfc9c8a6f318eefe9ee74726
Sheetal <sheetalrana190@gmail.com>
2025-02-03 12:14:57 -0500

    Update: Added library class

---
+++ TriangleTest/Class1.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+using IsoselesTriangle;
+namespace TriangleTest
+{
+    [TestFixture]
+    public class Class1
+    {
+        [Test]
+        public void ValidTriangle_Input5and5and7_ReturnsValidTriangle()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 5;
+            int thirdSide = 7;
+            string expected = "The triangl is Isosceles Triangle.";
+
+            // Act
+            string actual = Triangle.ValidTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void ValidTriangle_Input5and6and7_ReturnsNotValidTriangle()
+        {
+            // Arrange
+            int firstSide = 5;
+            int secondSide = 6;
+            int thirdSide = 7;
+            string expected = "The triangl is Isosceles Triangle.";
+
+            // Act
+            string actual = Triangle.ValidTriangle(firstSide, secondSide, thirdSide);
+
+            // Assert
+            ClassicAssert.AreEqual(expected, actual);
+        }
+    }
+}
