e934ea8d6155eb46169058797a2b3cfc3378863c
Sheetal <sheetalrana190@gmail.com>
2025-02-03 11:59:45 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace IsoselesTriangle
+{
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstSide, int secondSide, int thirdSide)
+        {
+            string result;
+            if ((firstSide == secondSide) || (firstSide == thirdSide) || (secondSide == thirdSide))
+            {
+                result = "The triangl is Isosceles Triangle.";
+            }
+            else
+            {
+                result = "The triangle is NOT Isosceles Triangle.";
+            }
+            return result;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstSide = 5;
+            int secondSide = 5;
+            int thirdSide = 7;
+            string result = Triangle.ValidTriangle(firstSide, secondSide, thirdSide);
+            Console.WriteLine(result);
+        }
+    }
+
+
+}
